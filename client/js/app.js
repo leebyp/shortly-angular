0a723a9b397ad6a2e01eecf931cbb11be0f97f0a
Benjamin Lee <leebyp@gmail.com>
2014-05-16 23:57:58 -0700

    Routing to home.html template works at url '/'.

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,17 @@
+var app = angular.module('app', ['ngRoute']);
 
+app.controller('HomeController', function($scope, $http){
+  $http.get('/links').success(function(data){
+    $scope.links = data;
+  })
+})
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: '/client/templates/home.html',
+    controller: 'HomeController'
+  })
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+}]);
